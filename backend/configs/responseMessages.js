b0a23e458e1965fae19fb80b0e57a1d1a9fa795f
Keshav Agrawal <keshava.bca2022@ssism.org>
2024-05-08 16:14:54 +0530

    response messages fixed

--- backend/configs/responseMessages.js
+++ backend/configs/responseMessages.js
@@ -1,31 +1,13 @@
 const responseMessages = {
-  USER_EXISTS: {
-    message: "User with this email already exists.",
-  },
-  USER_REGISTRATION_SUCCESS: {
-    message: "User registered successfully.",
-  },
-  USER_NOT_FOUND: {
-    message: "User with this email is not registered.",
-  },
-  INVALID_PASSWORD: {
-    message: "Invalid password. Please try again.",
-  },
-  USER_LOGIN_SUCCESS: {
-    message: "User logged in successfully.",
-  },
-  OTP_SENT_SUCCESS: {
-    message: "Password reset OTP sent to your email.",
-  },
-  OTP_VERIFIED: {
-    message: "OTP verified.",
-  },
-  PASSWORD_UPDATE_SUCCESS: {
-    message: "Password updated successfully.",
-  },
-  INTERNAL_SERVER_ERROR: {
-    message: "Internal server error",
-  },
+  USER_EXISTS: "User with this email already exists.",
+  USER_REGISTRATION_SUCCESS: "User registered successfully.",
+  USER_NOT_FOUND: "User with this email is not registered.",
+  INVALID_PASSWORD: "Invalid password. Please try again.",
+  USER_LOGIN_SUCCESS: "User logged in successfully.",
+  OTP_SENT_SUCCESS: "Password reset OTP sent to your email.",
+  OTP_VERIFIED: "OTP verified.",
+  PASSWORD_UPDATE_SUCCESS: "Password updated successfully.",
+  INTERNAL_SERVER_ERROR: "Internal server error",
 };
 
 module.exports = responseMessages;
